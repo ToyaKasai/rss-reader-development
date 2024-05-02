5ed839510dc68b2c7b4ebe5c00662a8b25ffdd06
ToyaKasai <kasaitoya@team-lab.com>
2024-05-03 00:06:57 +0900

    build: setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import eslintJs from '@eslint/js';
+import eslintTs from 'typescript-eslint';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+import globals from 'globals';
+
+export default eslintTs.config(
+  eslintJs.configs.recommended,
+  ...eslintTs.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+  },
+  eslintConfigPrettier,
+);
